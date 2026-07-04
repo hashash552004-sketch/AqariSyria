a8965ee94808fb1e1f78c213c8fcc4762c820db5
hashash552004-sketch <hashash552004@gmail.com>
2026-07-04 11:17:27 +0300

    تجميل الواجهات: تصميم MD3 بزوايا 28dp، شريط بحث في الرئيسية، تحسين كروت العقارات

--- app/src/main/java/com/aqarisyria/app/fragments/HomeFragment.java
+++ app/src/main/java/com/aqarisyria/app/fragments/HomeFragment.java
@@ -50,6 +50,10 @@ public class HomeFragment extends Fragment {
         loadFeaturedProperties();
         loadRecentProperties();
 
+        binding.searchBar.setOnClickListener(v -> {
+            if (isAdded()) startActivity(new Intent(getActivity(), SearchActivity.class));
+        });
+
         binding.tvShowAllFeatured.setOnClickListener(v -> {
             if (isAdded()) startActivity(new Intent(getActivity(), SearchActivity.class));
         });
